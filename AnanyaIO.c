c1bc6813f15f125a022db6a5d554ca3b4a842a16
manjiripathak16 <59915541+manjiripathak16@users.noreply.github.com>
2020-02-09 11:09:24 +0530

    Expression Evaluator

--- AnanyaIO.c
+++ AnanyaIO.c
@@ -148,3 +148,45 @@ struct node *IntoPost(char infix[], struct node *pstart)
 	pstart = DisplayPost(pstart);
 	return pstart;
 }
+
+//PART 2
+// Stack type
+struct Stack 
+{ 
+    int top; 
+    unsigned capacity; 
+    int* array; 
+}; 
+  
+// Stack Operations 
+struct Stack* createStack( unsigned capacity ) 
+{ 
+    struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack)); 
+  
+    if (!stack) return NULL; 
+  
+    stack->top = -1; 
+    stack->capacity = capacity; 
+    stack->array = (int*) malloc(stack->capacity * sizeof(int)); 
+  
+    if (!stack->array) return NULL; 
+  
+    return stack; 
+} 
+  
+int isEmpty(struct Stack* stack) 
+{ 
+    return stack->top == -1 ; 
+} 
+  
+int spop(struct Stack* stack) 
+{ 
+    if (!isEmpty(stack)) 
+        return stack->array[stack->top--] ; 
+    return 0; 
+} 
+  
+void spush(struct Stack* stack,int op) 
+{ 
+    stack->array[++stack->top] = op; 
+} 
